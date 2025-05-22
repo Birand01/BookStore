53eb7e36099369a653155611736c681649cba515
Birand Kılınç <pacmaidanapac@gmail.com>
2025-05-22 23:59:21 +0300

    Http Requests are implemented

--- BackEnd/Controllers/BookController.cs
+++ BackEnd/Controllers/BookController.cs
@@ -3,13 +3,138 @@ using BackEnd.Data;
 using BackEnd.Models;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Extensions;
 
 namespace BackEnd.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BookController:ControllerBase
+    public class BookController : ControllerBase
     {
-       
+        private readonly ApplicationDbContext _context;
+        public BookController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            try
+            {
+                var books = _context.Books.ToList();
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var book = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+                if (book is null)
+                {
+                    return NotFound(); // 404
+                }
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+            try
+            {
+
+                if (book is null)
+                {
+                    return NotFound(); // 404
+                }
+                _context.Books.Add(book);
+                _context.SaveChanges();
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
+        {
+            try
+            {
+                var entity = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+                if (entity is null)
+                    return NotFound();
+
+                if (id != book.Id)
+                {
+                    return BadRequest();
+                }
+                entity.Title = book.Title;
+                entity.Price = book.Price;
+                _context.SaveChanges();
+                return Ok(book);
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOneBooks([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var entity = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+
+                if (entity is null)
+                {
+                    return NotFound();
+                }
+                _context.Books.Remove(entity);
+                _context.SaveChanges();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        [HttpPatch("{id:int}")]
+        public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id,
+        [FromBody] JsonPatchDocument<Book> bookPatch)
+        {
+             try
+            {
+                var entity = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+
+                if (entity is null)
+                {
+                    return NotFound();
+                }
+                bookPatch.ApplyTo(entity);
+                _context.SaveChanges();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
     }
 }
