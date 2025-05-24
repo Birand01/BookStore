b05554f17c9fc17e7deda76a84bb323714eef7bd
Birand Kılınç <pacmaidanapac@gmail.com>
2025-05-24 10:43:37 +0300

    ReposityoryBase class is introduced

---
+++ BackEnd/Repositories/RepositoryBase.cs
@@ -0,0 +1,33 @@
+using BackEnd.Repositories.Contracts;
+
+namespace BackEnd.Repositories
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindAll(bool trackChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindByCondition(System.Linq.Expressions.Expression<Func<T, bool>> expression, bool trackChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
