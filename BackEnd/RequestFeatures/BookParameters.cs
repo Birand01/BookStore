b78bc30ad583f71da0182933ada423f3ae7ff027
Birand Kılınç <pacmaidanapac@gmail.com>
2025-05-26 21:54:37 +0300

    .

--- BackEnd/RequestFeatures/BookParameters.cs
+++ BackEnd/RequestFeatures/BookParameters.cs
@@ -8,6 +8,7 @@ namespace BackEnd.RequestFeatures
         public bool ValidPriceRange => MaxPrice > MinPrice;
 
         public string? SearchTerm { get; set; }
+        public string? OrderBy { get; set; }
 
 
         public BookParameters()
