7fd47fea636d7578ba9c0bdbcd833f13e2b878e7
Birand Kılınç <pacmaidanapac@gmail.com>
2025-05-26 10:57:17 +0300

    PagedList is implemented

--- BackEnd/RequestFeatures/MetaData.cs
+++ BackEnd/RequestFeatures/MetaData.cs
@@ -2,6 +2,13 @@ namespace BackEnd.RequestFeatures
 {
     public class MetaData
     {
-        
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+
     }
 }
